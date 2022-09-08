e4881bdeaefe56834f9c6a8f6e7f506afd98aa16
keelcoutinho <kelvia_cou@hotmail.com>
2022-09-08 16:14:38 -0300

    First component

---
+++ frontend/src/components/NotificationButtom/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/notification-icon.svg'
+import './style.css'
+function NotificationButtom() {
+    return (
+
+        <div className="meta-red-btn">
+            <img src={icon} alt="notificar"/>            
+        </div>
+
+    )
+}
+
+export default NotificationButtom

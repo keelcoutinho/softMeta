881503ec96f6e5d532f1e055534a24f2c345e9fe
keelcoutinho <kelvia_cou@hotmail.com>
2022-09-08 11:53:05 -0300

    Project created

---
+++ backend/softmeta/src/main/java/com/soft/softmeta/SoftmetaApplication.java
@@ -0,0 +1,13 @@
+package com.soft.softmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftmetaApplication.class, args);
+	}
+
+}
---
+++ backend/softmeta/src/test/java/com/soft/softmeta/SoftmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft.softmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
